1553d7bfaf92de97b54bb79d1c8aa255a488e0b3
Tracy <tracy@Tracys-Laptop.local>
2025-07-18 15:52:12 -0400

    更新publications生成器：使用新的论文内容

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -24,6 +24,7 @@
 # In[2]:
 
 import pandas as pd
+import os
 
 
 # ## Import TSV
@@ -34,8 +35,8 @@ import pandas as pd
 
 # In[3]:
 
-publications = pd.read_csv("publications.tsv", sep="\t", header=0)
-publications
+# Read TSV file
+publications = pd.read_csv("publications.tsv", sep='\t', header=0)
 
 
 # ## Escape special characters
@@ -61,48 +62,19 @@ def html_escape(text):
 
 # In[5]:
 
-import os
 for row, item in publications.iterrows():
     
-    md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
-    html_filename = str(item.pub_date) + "-" + item.url_slug
+    md_filename = os.path.basename(item.url_slug)
+    html_filename = md_filename
     year = item.pub_date[:4]
     
-    ## YAML variables
+    md = "---\ntitle: \"{}\"\ncollection: publications\npermalink: /publication/{}\nvenue: '{}'\ncitation: '{}'\n---".format(item.title, html_filename, item.venue, item.citation)
     
-    md = "---\ntitle: \""   + item.title + '"\n'
+    # Create directory if it doesn't exist
+    if not os.path.exists('../_publications'):
+        os.makedirs('../_publications')
     
-    md += """collection: publications"""
-    
-    md += """\npermalink: /publication/""" + html_filename
-    
-    if len(str(item.excerpt)) > 5:
-        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
-    
-    md += "\ndate: " + str(item.pub_date) 
-    
-    md += "\nvenue: '" + html_escape(item.venue) + "'"
-    
-    if len(str(item.paper_url)) > 5:
-        md += "\npaperurl: '" + item.paper_url + "'"
-    
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
-    
-    md += "\n---"
-    
-    ## Markdown description for individual page
-    
-    if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
-        
-    if len(str(item.excerpt)) > 5:
-        md += "\n" + html_escape(item.excerpt) + "\n"
-        
-    md += "\nRecommended citation: " + item.citation
-    
-    md_filename = os.path.basename(md_filename)
-       
-    with open("../_publications/" + md_filename, 'w') as f:
+    with open("../_publications/" + md_filename + ".md", 'w') as f:
         f.write(md)
 
 
